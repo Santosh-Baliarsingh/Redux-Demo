9d4ac6001db15b9c155c4ad8f7635d731949fca1
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-09-02 19:44:58 +0530

    Store.js File Updated With IceCreamReducer

--- Redux-Toolkit/app/store.js
+++ Redux-Toolkit/app/store.js
@@ -1,11 +1,17 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
-const cakeReducer = require("../features/cake/cakeSlice");
+const cakeReducer = require("../features/cake/cakeSlice"); // Cake reducer imported
+const iceCreamReducer = require("../features/iceCream/iceCreamSlice"); // IceCream reducer imported
 
 // Redux-Toolkit Store Creation
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
+    iceCream: iceCreamReducer,
   },
 });
 
 module.exports = store;
+
+// Note :-
+// 1 - In Redux if we have multiple reducer then we combine those reduers into a rootReducer.
+// 2 - In Redux-Tookit configureStore() method will handle that
