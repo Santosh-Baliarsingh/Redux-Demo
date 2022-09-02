3565f82b71eefe58eca76039293764563aee1b76
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-09-02 19:45:52 +0530

    Index.js File Updated with IceCreamActions

--- Redux-Toolkit/index.js
+++ Redux-Toolkit/index.js
@@ -1,19 +1,25 @@
 const store = require("./app/store"); //  import the store from store.js
 const { cakeActions } = require("./features/cake/cakeSlice"); // Import named cakeActions from CakeSlice
+const { iceCreamActions } = require("./features/iceCream/iceCreamSlice"); // Imported Named IceCreamActions from IceCreamSlice
 
-// Log the Initial State of Cake
-console.log("Initial State of Cake is -", store.getState());
+// Log the Initial State of Cake and IceCream
+console.log("Initial State of Features are -", store.getState());
 
-
-// Log the Updated State of Cake After Changes
+// Log the Updated State of Cake and IceCream After Changes
 const unsubscribe = store.subscribe(() => {
-  console.log("Updated State of Cake is -", store.getState());
+  console.log("Updated State of Features are -", store.getState());
 });
 
-// Dispatch Actions
+// Dispatch Actions for Cake
 store.dispatch(cakeActions.ordered()); // 9
 store.dispatch(cakeActions.ordered()); // 8
 store.dispatch(cakeActions.ordered()); // 7
 store.dispatch(cakeActions.restocked(10)); // 17
+
+// Dispatch Actions for Ice-Cream
+store.dispatch(iceCreamActions.ordered()); //19
+store.dispatch(iceCreamActions.ordered()); //18
+store.dispatch(iceCreamActions.ordered()); //17
+store.dispatch(iceCreamActions.restocked(10)); // 27
 
 unsubscribe();
